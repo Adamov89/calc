4714ebc9ea82411583018b055a3495b7445a226b
Adamov89 <40773158+Adamov89@users.noreply.github.com>
2019-06-23 16:52:28 +0300

    !C

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+
+namespace Calc_2
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Кулькулятор");
+            while (true)
+            {
+                double a, b, c; char h; c = 0.0;
+                Console.WriteLine("ввести первое число"); a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("ввести оператор (+) (-) (*) (/) "); h = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("ввести второе число"); b = Convert.ToDouble(Console.ReadLine());
+                switch (h)
+                {
+                    case '+':
+                        c = a + b;
+                        break;
+                    case '-':
+                        c = a - b;
+                        break;
+                    case '/':
+                        c = a / b;
+                        break;
+                    case '*':
+                        c = a * b;
+                        break;
+                }
+                Console.WriteLine();
+                Console.WriteLine("= " + c);
+                Console.WriteLine("Начать заново? (y/n)");
+                ConsoleKeyInfo e = Console.ReadKey();
+                if (e.KeyChar == 'n') break;
+                Console.WriteLine();
+
+            }
+
+        }
+    }
+}
+        
+    
+
